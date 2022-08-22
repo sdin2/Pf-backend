b95a860266330a2d203035a0f53f3af9ab8ca6c2
sdin2 <desat_94@hotmail.com>
2022-08-22 17:53:40 -0300

    new models and routes
    -added game model
    -added genre model
    -added game routes
    -added genre routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,17 @@
-require('dotenv').config();
-const { Router } = require('express');
-const { auth } = require('express-openid-connect');
+require("dotenv").config();
+const { Router } = require("express");
+const { auth } = require("express-openid-connect");
 const router = Router();
-const newsRoute = require('./newsRoute');
-const userRoute = require('./userRoute');
-const loginRoute = require('./loginRoute');
+const newsRoute = require("./newsRoute");
+const userRoute = require("./userRoute");
+const loginRoute = require("./loginRoute");
+const genreRoute = require("./genreRoute");
+const gamesRoute = require("./gamesRoute");
 
-router.use('/news', newsRoute);
-router.use('/users', userRoute);
-router.use('/login', loginRoute)
+router.use("/news", newsRoute);
+router.use("/users", userRoute);
+router.use("/login", loginRoute);
+router.use("/genre", genreRoute);
+router.use("/games", gamesRoute);
 
 module.exports = router;
