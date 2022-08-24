cbe31f3cdc48279e9e1af7c63f722918d34a0504
sdin2 <desat_94@hotmail.com>
2022-08-24 09:58:51 -0300

    modificado ruta, modelo y gamecontroller

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+const { saveAllGamesInDb } = require("./src/controllers/GamesController");
 let port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
+    saveAllGamesInDb();
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
