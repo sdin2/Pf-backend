70ffde39970f8a7764e11e8f287fceb32d20720a
TotiFz <totifernandez10@gmail.com>
2022-08-25 18:29:41 -0300

    Rutas Para Comentarios

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,24 +3,24 @@ const { Router } = require('express');
 const { auth } = require('express-openid-connect');
 const router = Router();
 
-const newsRoute = require("./newsRoute");
-const userRoute = require("./userRoute");
-const loginRoute = require("./loginRoute");
-const genreRoute = require("./genreRoute");
-const gamesRoute = require("./gamesRoute");
-const forumRoute = require("./forumRoute");
+const newsRoute = require('./newsRoute');
+const userRoute = require('./userRoute');
+const loginRoute = require('./loginRoute');
+const genreRoute = require('./genreRoute');
+const gamesRoute = require('./gamesRoute');
+const forumRoute = require('./forumRoute');
 const rewardRoute = require('./rewardRoute');
-const missionsRoute = require ('./missionsRoute')
+const missionsRoute = require('./missionsRoute');
+const forumCommentsRoutes = require('./forumCommentsRoute');
 
-router.use("/news", newsRoute);
-router.use("/users", userRoute);
-router.use("/login", loginRoute);
-router.use("/genre", genreRoute);
-router.use("/games", gamesRoute);
-router.use("/forum", forumRoute);
+router.use('/news', newsRoute);
+router.use('/users', userRoute);
+router.use('/login', loginRoute);
+router.use('/genre', genreRoute);
+router.use('/games', gamesRoute);
+router.use('/forum', forumRoute);
 router.use('/reward', rewardRoute);
-router.use('/missions', missionsRoute)
-
-
+router.use('/missions', missionsRoute);
+router.use('/anwsers', forumCommentsRoutes);
 
 module.exports = router;
