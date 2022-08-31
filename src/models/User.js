b731514677115919d1ce55be61f73bb3e97bb246
sdin2 <desat_94@hotmail.com>
2022-08-31 13:37:36 -0300

    ahora funciona igual a heroku

--- src/models/User.js
+++ src/models/User.js
@@ -1,83 +1,83 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
-module.exports = sequelize => {
-	sequelize.define('user', {
-		email: {
-			type: DataTypes.STRING,
-			allownull: false,
-			unique: true,
-		},
-		id: {
-			type: DataTypes.UUID,
-			defaultValue: DataTypes.UUIDV4,
-			allowNull: false,
-			primaryKey: true,
-		},
-		nickname: {
-			type: DataTypes.STRING,
-			allownull: false,
-			unique: true,
-		},
-		img: {
-			type: DataTypes.STRING,
-			allownull: true,
-			defaultValue: 'https://j.gifs.com/ygdY27.gif',
-		},
-		deleteFlag: {
-			type: DataTypes.BOOLEAN,
-			allownull: true,
-			defaultValue: false,
-		},
-		bannedFlag: {
-			type: DataTypes.BOOLEAN,
-			allownull: true,
-			defaultValue: false,
-		},
-		password: {
-			type: DataTypes.STRING,
-			allownull: true,
-		},
-		matched_users: {
-			type: DataTypes.JSON,
-			defaultValue: [],
-			allownull: true,
-		},
-		coins: {
-			type: DataTypes.INTEGER,
-			allownull: true,
-			defaultValue: 0,
-		},
-		favoriteGames: {
-			type: DataTypes.JSON,
-			defaultValue: [],
-		},
-		servers: {
-			type: DataTypes.STRING,
-			defaultValue: '',
-		},
-		missionCompleted: {
-			type: DataTypes.JSON,
-			defaultValue: [],
-		},
-		isAdmin: {
-			type: DataTypes.JSON,
-			defaultValue: { user: true, admin: false, superAdmin: false },
-		},
-		rating: {
-			type: DataTypes.INTEGER,
-			defaultValue: 0,
-		},
-		plan: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: false,
-		},
-		wantsMatch: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: false,
-		},
-		description: {
-			type: DataTypes.STRING,
-			defaultValue: 'Bienvenido a mi perfil!',
-		},
-	});
+module.exports = (sequelize) => {
+  sequelize.define("user", {
+    email: {
+      type: DataTypes.STRING,
+      allownull: false,
+      unique: true,
+    },
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    nickname: {
+      type: DataTypes.STRING,
+      allownull: false,
+      unique: true,
+    },
+    img: {
+      type: DataTypes.STRING,
+      allownull: true,
+      defaultValue: "https://j.gifs.com/ygdY27.gif",
+    },
+    deleteFlag: {
+      type: DataTypes.BOOLEAN,
+      allownull: true,
+      defaultValue: false,
+    },
+    bannedFlag: {
+      type: DataTypes.BOOLEAN,
+      allownull: true,
+      defaultValue: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allownull: true,
+    },
+    matched_users: {
+      type: DataTypes.JSON,
+      defaultValue: [],
+      allownull: true,
+    },
+    coins: {
+      type: DataTypes.INTEGER,
+      allownull: true,
+      defaultValue: 0,
+    },
+    favoriteGames: {
+      type: DataTypes.JSON,
+      defaultValue: [],
+    },
+    servers: {
+      type: DataTypes.STRING,
+      defaultValue: "",
+    },
+    missionCompleted: {
+      type: DataTypes.JSON,
+      defaultValue: [],
+    },
+    isAdmin: {
+      type: DataTypes.JSON,
+      defaultValue: { user: true, admin: false, superAdmin: false },
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    plan: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    wantsMatch: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      defaultValue: "Bienvenido a mi perfil!",
+    },
+  });
 };
