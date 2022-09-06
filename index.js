e89c0db4d4deb8dc87413ec95d9b3c77a02e7298
Mateo <msolat01@gmail.com>
2022-09-06 15:43:21 -0300

    mailController, emailRoute

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const socketServerIo = require("./src/app.js");
-const { conn } = require("./src/db.js");
-const { saveAllGamesInDb } = require("./src/controllers/GamesController");
+const socketServerIo = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const { saveAllGamesInDb } = require('./src/controllers/GamesController');
 let PORT = 3001;
 
 // Syncing all the models at once.
-conn.sync().then(() => {
-  socketServerIo.listen(3001, () => {
-    saveAllGamesInDb();
-    console.log(`%s listening at 3001`); // eslint-disable-line no-console
-  });
+conn.sync({ force: true }).then(() => {
+	socketServerIo.listen(3001, () => {
+		saveAllGamesInDb();
+		console.log(`%s listening at 3001`); // eslint-disable-line no-console
+	});
 });
